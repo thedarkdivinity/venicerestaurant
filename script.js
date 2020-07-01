a8cc458ac310f74a6959651ef48b9e848dcc1b39
Sayush Kamat <thedarkdivinity@users.noreply.github.com>
2020-07-01 22:01:24 +0530

    landing page completed

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
+AOS.init();
+
+
 $('.counter').counterUp({
     delay: 10,
     time: 5000
 });
+
+$("#ApplyOverlay").sliphover({
+    
+    
+});
+$("#ApplyOverlayPasta").sliphover({
+    
+    
+});
+$("#ApplyOverlayPizza").sliphover({
+    
+    
+});
+$("#ApplyOverlayNoodle").sliphover({
+    
+    
+});
+
